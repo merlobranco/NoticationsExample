403ed55d336ea68a7e8a5872fdc93659a215aae2
Brais Cidras <braisomary@gmail.com>
2020-08-04 12:17:34 +0200

    Adding Notification Channels and creating custom notifications

---
+++ app/src/main/java/com/sample/notificationsexample/App.kt
@@ -0,0 +1,51 @@
+package com.sample.notificationsexample
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+
+/**
+ * Wrapping our whole application with all its all components like activities and services
+ * Required for setting up at the start launch of our application no at the in a particular activity
+ */
+
+// Setting notification channels
+const val CHANNEL_1_ID = "channel1"
+const val CHANNEL_2_ID = "channel2"
+
+class App: Application() {
+
+    /**
+     * This will be called before any activity starts.
+     * It will start with our App.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannels()
+    }
+
+    private fun createNotificationChannels() {
+        // Checking if our SDK is greater than Oreo
+        // Notification Channel class is not available in lower versions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel1 = NotificationChannel(
+                CHANNEL_1_ID,
+                getString(R.string.channel1_name),
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel1.description = getString(R.string.channel1_description)
+
+            val channel2 = NotificationChannel(
+                CHANNEL_2_ID,
+                getString(R.string.channel2_name),
+                NotificationManager.IMPORTANCE_LOW
+            )
+            channel2.description = getString(R.string.channel2_description)
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel1)
+            notificationManager.createNotificationChannel(channel2)
+        }
+    }
+}
--- app/src/main/java/com/sample/notificationsexample/MainActivity.kt
+++ app/src/main/java/com/sample/notificationsexample/MainActivity.kt
@@ -2,11 +2,60 @@ package com.sample.notificationsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var notificationManager: NotificationManagerCompat
+    private lateinit var editTextTitle: EditText
+    private lateinit var editTextMessage: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        notificationManager = NotificationManagerCompat.from(this)
+
+        editTextTitle = findViewById(R.id.edit_text_title)
+        editTextMessage = findViewById(R.id.edit_text_message)
+    }
+
+    fun sendOnChannel1(v: View) {
+        val title = editTextTitle.text.toString()
+        val message = editTextMessage.text.toString()
+
+        // The notification channel configuration could/should be set here if we are working with an API lower than Oreo
+        // Since here we could override the notification channel properties
+        // The provided channel ID will be ignored in APIs lower than Oreo
+        // (No version checking is required, it won't crash)
+        var notification = NotificationCompat.Builder(this, CHANNEL_1_ID)
+            .setSmallIcon(R.drawable.ic_one)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+            .build()
+
+        // If we want to send different notifications at the same time we should provide different ids
+        notificationManager.notify(1, notification)
+    }
+
+    fun sendOnChannel2(v: View) {
+        val title = editTextTitle.text.toString()
+        val message = editTextMessage.text.toString()
+
+        var notification = NotificationCompat.Builder(this, CHANNEL_2_ID)
+            .setSmallIcon(R.drawable.ic_two)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .build()
+
+        // Providing a new id in order to not override the Notification One
+        notificationManager.notify(2, notification)
+
     }
 }
